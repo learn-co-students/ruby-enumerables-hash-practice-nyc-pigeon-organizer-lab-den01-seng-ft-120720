7c5999b167c805b8f97586da0b422863b4cdab95
Jack Hubert <jphubert93@gmail.com>
2020-11-25 18:29:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Create a list of all pigeon names
+  data.each_with_object({}) do |(key, value), final_array|
+    # binding.pry
+    value.each do |nested_key, names|
+      # binding.pry
+      names.each do |name|
+        # binding.pry
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          final_array[name][key] = []
+        end
+        final_array[name][key] << nested_key.to_s
+        # binding.pry
+      end
+    end
+   end
+  # binding.pry
 end
